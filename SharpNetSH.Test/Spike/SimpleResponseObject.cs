477a0b74dc002b201bce239daff26bc997d85aec
Robert Petz <rpetz2007@gmail.com>
2015-02-09 21:59:31 -0800

    straggler file

---
+++ SharpNetSH.Test/Spike/SimpleResponseObject.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+
+namespace Ignite.SharpNetSH.Test.Spike
+{
+	public class SimpleResponseObject : IResponseProcessor
+	{
+		internal SimpleResponseObject()
+		{
+			
+		}
+
+		object IResponseProcessor.ProcessResponse(IEnumerable<string> responseLines)
+		{
+			return this;
+		}
+	}
+}
