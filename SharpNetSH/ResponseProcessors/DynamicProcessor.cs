b1067258a8c6d5ab23fd7105257b0e4fa0f8404d
Robert Petz <rpetz2007@gmail.com>
2015-02-12 11:24:08 -0800

    Migrated everything to convention over configuration - still need to ensure the correct response processors are assinged to each method on each interface, but no longer are we defining each response object for each method

---
+++ SharpNetSH/ResponseProcessors/DynamicProcessor.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace Ignite.SharpNetSH
+{
+	internal class DynamicProcessor : IResponseProcessor
+	{
+		StandardResponse IResponseProcessor.ProcessResponse(IEnumerable<string> responseLines, int exitCode)
+		{
+			//TODO: Process the response into a dynamic object
+			throw new System.NotImplementedException();
+		}
+	}
+}
